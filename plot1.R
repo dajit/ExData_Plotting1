4300743fb1edb35de6366ff5e02c77db5ac9dbd1
ajit_dhamale <dajit@users.noreply.github.com>
2016-08-14 14:44:23 -0400

    Add files via upload
    project plot 1 files

---
+++ plot1.R
@@ -0,0 +1,33 @@
+# Load required library
+library(data.table)
+library(base)
+
+setwd("C:\\coursera\\course4\\wk1\\project")
+
+idt <- fread("household_power_consumption.txt", sep = ";")
+
+# note must ignore NA when sum
+sum(as.numeric(t1$Global_active_power), na.rm = TRUE)
+# big object 150 MB
+object.size(idt)
+
+# get only the data forrequired dates
+dt1 <- idt[(idt$Date == "1/2/2007" ),]
+dt2 <- idt[(idt$Date == "2/2/2007" ),]
+
+# merge
+td <- rbind(dt1,dt2)
+
+# much smaller data set
+#object.size(td)
+# 394168 bytes
+
+# Plot 1
+hist(as.numeric(td$Global_active_power), col = "red" , xlab = "Global Active Power (kilowatts)", main = ("Global Active Power") )
+
+## Copy my plot to a PNG file
+dev.copy(png, file = "plot1.png")
+
+## Don't forget to close the PNG device!
+dev.off()
+
